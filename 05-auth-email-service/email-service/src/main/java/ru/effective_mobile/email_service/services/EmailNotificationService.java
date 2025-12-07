51f288708ef8130776449c1993a733512cfeb296
YuzhakovMikhail <miha.yuzhakov@gmail.com>
2025-12-07 20:42:00 +0300

    Hometask05: AuthEmailService.

--- 05-auth-email-service/email-service/src/main/java/ru/effective_mobile/email_service/services/EmailNotificationService.java
+++ 05-auth-email-service/email-service/src/main/java/ru/effective_mobile/email_service/services/EmailNotificationService.java
@@ -9,21 +9,14 @@ import ru.effective_mobile.email_service.dto.UserRegisteredEvent;
 @Slf4j
 public class EmailNotificationService {
 
-    // Временно принимаем String, а не DTO
-    @KafkaListener(topics = "user-registration-topic", groupId = "email-debug-group")
-    public void listen(String message) {
-        log.info("!!! СЫРОЕ СООБЩЕНИЕ ПОЛУЧЕНО: {}", message);
-    }
+    @KafkaListener(topics = "user-registration-topic", groupId = "email-service-group")
+    public void listen(UserRegisteredEvent event) {
+        log.info("Получено событие регистрации для email: {}", event.getEmail());
 
-//    @KafkaListener(topics = "user-registration-topic", groupId = "email-service-group")
-//    public void listen(UserRegisteredEvent event) {
-//        // Здесь вся бизнес-логика email сервиса
-//        log.info("Получено событие регистрации для email: {}", event.getEmail());
-//
-//        // Имитация отправки письма
-//        System.out.println("------------------------------------------------");
-//        System.out.println("EMAIL SERVICE: Письмо отправлено на " + event.getEmail());
-//        System.out.println("EMAIL SERVICE: Ваш код подтверждения: " + event.getVerificationCode());
-//        System.out.println("------------------------------------------------");
-//    }
+        // Имитация отправки письма
+        System.out.println("------------------------------------------------");
+        System.out.println("EMAIL SERVICE: Письмо отправлено на " + event.getEmail());
+        System.out.println("EMAIL SERVICE: Ваш код подтверждения: " + event.getVerificationCode());
+        System.out.println("------------------------------------------------");
+    }
 }
